7b1475c5148056cc74b16c7f56de12313e8bda67
nd <ndodanli14@gmail.com>
2021-09-02 21:44:38 +0300

    port changed

--- src/main/java/com/karserver/KAR/APP/Server.java
+++ src/main/java/com/karserver/KAR/APP/Server.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class Server {
 
-    static final int PORT = 8080;
+    static final int PORT = 5000;
     public static ArrayList<EchoThread> threads;
     public static int index;
 
